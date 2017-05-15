9fe94d5d32273bc2532568ad87efd110b4fb84d9
Joshua T Corbin <jcorbin@wunjo.org>
2017-05-15 08:00:16 -0700

    Further minimize example buffer

--- 0.go
+++ 0.go
@@ -1,12 +1,9 @@
-// To reproduce, run the following in normal mode: (requires tpope unimpaired `[e` exchange binding)
-//   9ggf r<Enter>k[eJA;<Esc>JjVj]e]ece
+// run the following in normal mode: 5ggJjVj]e]ece
 package main
 
-func bar() (int, error)
-
 func foo() error {
-	n, err := bar()
-	if err != nil {
+	if n, err := bar();
+	err != nil {
 		return err
 	}
 	n = n * n
